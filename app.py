c256f4836f6392da065578da082757110701ebaf
ze2 <62995130+ze-2@users.noreply.github.com>
2022-06-12 23:22:48 +0800

    done

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, redirect, render_template, request, url_for, redirect
 from random import randint
 import sqlite3
@@ -6,7 +7,9 @@ entries = [0]
 encouragements_entries = [0]
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 
+triggers = ['kill', 'suicide', 'sh', 'self-harm', 'depressed', 'die']
 
 @app.route('/')
 def index():
@@ -37,6 +40,11 @@ def submit(entries=entries, encouragements=encouragements_entries):
                     "INSERT INTO messages(id, message, contact) VALUES (?, ?, ?)", (entries[0], message, contact))
                 con.commit()
             entries[0] += 1
+        messages = message.split(' ')
+        for word in messages:
+            if word.lower() in triggers:
+                return render_template('help.html')
+
         return redirect(url_for('index'))
     else:
         return render_template('submit.html')
@@ -64,6 +72,10 @@ def encouragements():
             "SELECT * FROM encouragements WHERE id = (?)", (encouragement,))
         rows = cur.fetchall()
         return render_template('encouragements.html', contact=rows[0]['contact'], message=rows[0]['message'])
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
